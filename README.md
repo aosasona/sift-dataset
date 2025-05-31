05cfc933686bd3d41ee4db7f1692fa114282436d
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-05-31 22:54:03 +0100

    Train model with CreateML

--- README.md
+++ README.md
@@ -1,25 +1,6 @@
 # Sift dataset
 
-This was for an Advance AI and Applications module.
-
-## Pre-requisites
-
-- uv
-- python (>=3.11)
-
-All scripts in this directory are executed using `uv` as in:
-
-```sh
-uv run ./process.py && uv run ./train.py
-```
-
-This takes care of all the dependencies automatically.
-
-# Models
-
-- The CoreML model is located in `./ArticlesClassifier.mlpackage`
-- The Keras model is located in `./articles_classifier/`
-- The `tflite` (not known as LiteRT) model (require Tensorflow bindings and ops enabled) is located in `./articles_classifier.tflite`
+The model was trained using the CreateML application with 70 iterations and Transfer Learning with BERT Embeddings.
 
 # Dataset
 
--- process.py
+++ process.py
@@ -12,6 +12,40 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 
+def merge_label(label: str) -> str:
+    label = label.strip().upper()
+
+    if label in ["AI", "COGNITIVE SCIENCE"]:
+        return "AI"
+
+    elif label in ["PROGRAMMING", "DEVOPS", "OPERATING SYSTEMS", "DESIGN", "COMPUTER SCIENCE"]:
+        return "ENGINEERING"
+
+    elif label in ["CRYPTOGRAPHY", "CYBERSECURITY"]:
+        return "SECURITY"
+
+    elif label in ["SCIENCE", "MATHEMATICS"]:
+        return "SCIENCE"
+
+    elif label in ["FINANCE", "EDUCATION", "LAW"]:
+        return "INDUSTRY"
+
+    elif label == "HARDWARE":
+        return "HARDWARE"
+
+    elif label in ["GAMES", "ART"]:
+        return "MEDIA"
+
+    elif label in ["UPDATES", "RETROSPECTIVE"]:
+        return "META"
+
+    elif label in ["RANT", "SATIRE", "CULTURE"]:
+        return "PERSPECTIVE"
+
+    else:
+        return label
+
+
 def clean_text(text: str) -> str:
     # Lowercase
     text = text.lower()
@@ -27,7 +61,7 @@ df = pd.read_json("./lobsters_dataset.json")
 
 # Create the excerpt column
 df["text"] = df["headline"] + " " + df["short_description"]
-df["text"] = df["text"].apply(clean_text)
+# df["text"] = df["text"].apply(clean_text)
 
 # print(df["text"].str.split().str.len().describe())
 
@@ -35,11 +69,13 @@ df["text"] = df["text"].apply(clean_text)
 df = df[["text", "category"]]
 
 # Merge similar categories
-df["category"] = df["category"]
+# df["category"] = df["category"]
+df["category"] = df["category"].apply(merge_label)
 
 # Encode the category column
 le = LabelEncoder()
-df["label"] = le.fit_transform(df["category"])
+df["label"] = df["category"].astype(str).apply(lambda x: x.strip().upper())
+df["label_int"] = le.fit_transform(df["category"])
 LABELS_LEN = len(le.classes_)
 
 # Save the label encoder for later use
@@ -49,5 +85,6 @@ with open(le_path, "w") as f:
     json.dump(le.classes_.tolist(), f)
 
 # Save the processed dataset
+df = df[["text", "label"]]
 df.to_csv("./dataset.csv", index=False)
 print("Dataset processed and saved successfully!")
